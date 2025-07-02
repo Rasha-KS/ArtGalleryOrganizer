96bc771d065415f4ff71697e16ee711cf7056841
mergit11 <emazum7@gmail.com>
2025-07-03 01:10:47 +0200

    update

--- ArtGalleryOrganizer/Forms/ArtistsManagement.cs
+++ ArtGalleryOrganizer/Forms/ArtistsManagement.cs
@@ -176,6 +176,7 @@ namespace ArtGalleryOrganizer
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+
             if (e.RowIndex >= 0)
             {
                 selectedRowIndex = e.RowIndex;
